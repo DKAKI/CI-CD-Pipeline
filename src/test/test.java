33bbb0499a6d318d1f22d3e4a076409b0fa3566f
DKAKI <50663884+DKAKI@users.noreply.github.com>
2020-12-21 19:56:49 +0100

    Update test.java

--- src/test/test.java
+++ src/test/test.java
@@ -8,28 +8,4 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;  
-public class example {  
-  
-    public static void main(String[] args) {  
-        
-    // declaration and instantiation of objects/variables  
-    System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");  
-    WebDriver driver=new ChromeDriver();  
-      
-// Launch website  
-    driver.get("http://google.com");  
-    File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        
-        //Copy the file to a location and use try catch block to handle exception
-        try {
-            FileUtils.copyFile(screenshot, new File("C:\\test\\test.png"));
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        
-        //closing the webdriver
-        driver.close();
-    }    
-
-      
-    }  
+ddddddddddddd
